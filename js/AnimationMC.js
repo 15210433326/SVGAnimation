892da26dcfd9579ab4d7fc2ac1ab1e7e21eebf54
DaVer <452202586@qq.com>
2015-07-31 19:25:07 +0800

    这是2.0版本的

--- js/AnimationMC.js
+++ js/AnimationMC.js
@@ -100,9 +100,6 @@
 			}
 
 			curDom.playAnimation(curDom.curFrame)
-
-
-
 			return this;
 		},
 		gotoAndPlay: function(num,loop) {
@@ -138,7 +135,7 @@
 						this.curFrame = this.startFrame;
 					}
 				}
-				
+				//console.log(this.curFrame)
 				this.playAnimation(this.curFrame);
 			}
 			
@@ -158,6 +155,106 @@
 		},
 		play:function(){
 			this.stop = false;
+			return this;
+		},
+		zdw_initColor:function(_KEY_FRAMES, TOTAL_FRAMES){
+			var KEY_FRAMES = _KEY_FRAMES;
+			var guanjian_frame = new Array(); //关键帧数据,比如第一帧  第五帧[1,5]
+			var guanjian_framedata = new Array(); //存放关键帧的具体数据的数组
+			var curDom = this;
+			
+			curDom.dataframe = {};
+			/**
+			 * 是否循环播放，默认为不循环播放
+			 */
+			curDom.loop = false;
+			/**
+			 * 当前帧
+			 */
+			curDom.curFrame = 1;
+			/**
+			 *总帧数 
+			 */
+			curDom.totalframe = TOTAL_FRAMES;
+			curDom.stoped = true;
+			curDom.endFrame = curDom.totalframe;
+			curDom.startFrame = 1;
+			curDom.increment = 1;//正方向还是反方向
+			
+			//console.log(KEY_FRAMES)
+			//获取各个关键帧的数据 数据作为一个数组，可以获得当前数组中所有关键帧
+			for (var i = 0; i < KEY_FRAMES.length; i++) {
+				for (var name in KEY_FRAMES[i]) {
+					guanjian_frame.push(parseInt(name.substr(2)))
+						//	console.log(KEY_FRAMES[i][name])
+				}
+			}
+
+//			console.log(guanjian_frame)
+			for (var j = 0; j < guanjian_frame.length; j++) {
+				//console.log(KEY_FRAMES[j]["fr"+guanjian_frame[j]+""])
+				guanjian_framedata.push(KEY_FRAMES[j]["fr" + guanjian_frame[j] + ""])
+			}
+
+
+			islastguanjian();
+
+			function islastguanjian() {
+				if (guanjian_frame[guanjian_frame.length - 1] <= TOTAL_FRAMES) {
+					guanjian_frame.push(TOTAL_FRAMES);
+					guanjian_framedata.push(guanjian_framedata[guanjian_framedata.length - 1])
+				}
+			}
+
+			createData();
+
+			//开始计算每个数组中数据的间隔
+			function createData() {
+				for (var i = 0; i < guanjian_frame.length - 1; i++) {
+					console.log("------------------")
+					add_KEY_FRAMES(i, i + 1);
+				}
+			}
+			
+			//向KEY_FRAMES对象中添加start到end的全部matrix数据
+			function add_KEY_FRAMES(start, end) {
+				var first = guanjian_framedata[start];
+				var last = guanjian_framedata[end];
+				var totframe = guanjian_frame[end] - guanjian_frame[start] + 1
+		
+				console.log(totframe)
+				
+				var firstnum10 = first[0].split(",")
+				var lastnum10 = last[0].split(",")
+				var jianger = (lastnum10[0]-firstnum10[0])/totframe;
+				var jiangeg = (lastnum10[1]-firstnum10[1])/totframe;
+				var jiangeb = (lastnum10[2]-firstnum10[2])/totframe;
+				
+				console.log(jianger+"++++++++++++++++++++")
+
+				for (var i = 0; i < totframe - 1; i++) {
+					var arrr =0;
+					var arrg =0;
+					var arrb =0;
+					arrr = (parseInt(firstnum10[0])+Math.round(parseFloat(jianger) * i));
+					arrg = parseInt(firstnum10[1])+Math.round(parseFloat(jiangeg) * i)
+					arrb = parseInt(firstnum10[2])+Math.round(parseFloat(jiangeb) * i)
+					console.log(parseInt(firstnum10[0])+"-----"+Math.round(parseFloat(jianger) * i)+"-------"+i)
+					curDom.dataframe['fr' + (i + 1 + guanjian_frame[start])] = "RGB("+arrr+","+arrg+","+arrb+")";
+						//console.log("RGB("+arrr,arrg,arrb+")")
+				}
+				curDom.dataframe['fr' + guanjian_frame[start]] = "RGB("+first[0]+")";
+				curDom.dataframe['fr' + guanjian_frame[end]] = "RGB("+last[0]+")";
+			}
+			curDom.playAnimation = function(cur){
+				console.log(curDom.dataframe["fr"+cur],cur)
+				$(curDom).css("stop-color",curDom.dataframe["fr"+cur])
+				console.log($(curDom).css("stop-color"))
+			}
+
+			curDom.playAnimation(curDom.curFrame)
+			
+			
 			return this;
 		}
 	})
